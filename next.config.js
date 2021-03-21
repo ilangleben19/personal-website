244b6c9bd00ee3ddb6ac09dce7a64364434e2dba
Ian Langleben <ilangleben19@gmail.com>
2021-03-21 19:24:43 -0400

    Add IT Officer platform shortlink

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/it-officer-platform',
+                destination: 'https://www.notion.so/ianlangleben/MSS-IT-Officer-Platform-Ian-Langleben-32cff4408fbb46aa96b66bac07b2d87f',
+                permanent: true,
+            },
+        ];
+    },
+};
