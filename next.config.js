831576b79de1a401d999a8d45f85d598c9003f56
Ian Langleben <ilangleben19@gmail.com>
2021-04-23 15:47:31 -0400

    Add link to recipes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ module.exports = {
     async redirects() {
         return [
             {
+                source: '/cookbook',
+                destination: 'https://www.notion.so/ianlangleben/4fa24719012346d9a6aa56f5f8ef260e?v=dd805850adb04d4b81916d4687993778',
+                permanent: true,
+            },
+            {
                 source: '/it-officer-platform',
                 destination: 'https://www.notion.so/ianlangleben/MSS-IT-Officer-Platform-Ian-Langleben-99b48d024730485c88f42375f569b9dd',
                 permanent: true,
@@ -9,6 +14,11 @@ module.exports = {
             {
                 source: '/med/it-officer-platform',
                 destination: 'https://www.notion.so/ianlangleben/MSS-IT-Officer-Platform-Ian-Langleben-99b48d024730485c88f42375f569b9dd',
+                permanent: true,
+            },
+            {
+                source: '/recipes',
+                destination: 'https://www.notion.so/ianlangleben/4fa24719012346d9a6aa56f5f8ef260e?v=dd805850adb04d4b81916d4687993778',
                 permanent: true,
             },
         ];
