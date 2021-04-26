a5e7eecb8eab0b8e6aba4ab1ec69db8071522847
Ian Langleben <ilangleben19@gmail.com>
2021-04-26 08:06:52 -0400

    Add Notion page as link

--- next.config.js
+++ next.config.js
@@ -17,6 +17,11 @@ module.exports = {
                 permanent: true,
             },
             {
+                source: '/med/notion',
+                destination: 'https://www.notion.so/ilangmed/Medical-school-9d735b5c46bd471e852fabd95ae7db87',
+                permanent: true,
+            },
+            {
                 source: '/recipes',
                 destination: 'https://www.notion.so/ianlangleben/4fa24719012346d9a6aa56f5f8ef260e?v=dd805850adb04d4b81916d4687993778',
                 permanent: true,
